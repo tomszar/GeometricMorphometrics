cd25a7477a527af664de1d331c1704f686b5574e
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2018-11-15 16:13:37 -0500

    updated code and gitignore

--- Code/2018-08-LandmarkProcessing.m
+++ Code/2018-08-LandmarkProcessing.m
@@ -1,56 +1,60 @@
-%This script will run a Generalized Procrustes Analysis
-%from the GPA function
-%Adding code
-addpath ('/home/tomas/Documents/Research/FacialSD/Code')
-%Add pkg
+% This script will run a Generalized Procrustes Analysis from the GPA function
+% and a PCA from the fastPCA function
+% First, we will keep only those individuals with genotype data, from the adapt_geno.fam file
+
+%Adding code from current directory
+addpath (pwd())
+
+%Load pkg
 pkg load statistics
 pkg load io
 pkg load geometry
-%Get folders
-folder.databases = '/home/tomas/Documents/Research/FacialSD/DataBases';
-folder.save      = '/home/tomas/Documents/Research/FacialSD/Results/FacePCA';
+
+%Get folders and load databases
+cd .. 
+folder.root      = pwd();
+folder.databases = strcat(folder.root, '/DataBases');
+folder.save      = strcat(folder.root, '/Results');
 cd(folder.databases);
 load('landmark_matrix.mat');
 load('landmark_ids.mat');
 load('landmark_facets.mat');
-%Loading IDs and groups from admixture k6 pop_struct 
-[ids, groups] = textread ('clusters_admix_k6.csv', '%s %f', 'delimiter' , ',');
-ids    = ids(2:end,:);
-groups = groups(2:end,:);
+%Loading IDs
+[~, geno_ids, ~,~,~,~] = textread ('adapt_geno.fam', '%s %s %f %f %f %f', 'delimiter' , ' ');
+%ids    = ids(2:end,:);
+%groups = groups(2:end,:);
 
-%Retain IDs from pop structure analysis into shape analysis
+%Retain IDs from genotype data into shape analysis
 %First, remove the string 'PSU' from the landmark_ids file, and remove the starting 0
 landmark_ids = erase(landmark_ids, 'PSU');
 landmark_ids = regexprep(landmark_ids,'^0*','');
 %Then, set the intersection
-[~ , keep] = intersect(landmark_ids, ids);
+[~ , keep] = intersect(landmark_ids, geno_ids);
 
 %Keep those only in the landmark_matrix
 landmark_matrix = landmark_matrix(keep,:);
-landmark_ids = landmark_ids(keep,:);
+landmark_ids    = landmark_ids(keep,:);
 
-[shape_matrix, cs] = GPA(landmark_matrix, 1);
-clear landmark_matrix;
 
 %PCA (Run this with random equal samples from each group)
 %Count group members
-[C,ia,ic] = unique(groups);
-group_counts  = accumarray(ic,1);
-group_counts
+%[C,ia,ic] = unique(groups);
+%group_counts  = accumarray(ic,1);
+%group_counts
 
 %Get random sample of 120 per group and run PCA on that
-row = 1
-sample = 120
-random_sample = zeros(size(C)(1) * sample, size(shape_matrix )(2));
-for i = 0:5
-  reduced_sample = shape_matrix(groups == i,:);
-  start = 1 + (i *sample);
-  fin   = row * sample;
-  random_sample(start:fin, :) = reduced_sample(randperm(sample), :);
-  row = row + 1;
-endfor
+%row = 1
+%sample = 120
+%random_sample = zeros(size(C)(1) * sample, size(shape_matrix )(2));
+%for i = 0:5
+%  reduced_sample = shape_matrix(groups == i,:);
+%  start = 1 + (i *sample);
+%  fin   = row * sample;
+%  random_sample(start:fin, :) = reduced_sample(randperm(sample), :);
+%  row = row + 1;
+%endfor
 %Run PCA on random sample
-[V,S,score,mean_eigenvals,index] = fastPCA(random_sample, 100, 1);
+[V,S,score,mean_eigenvals,index] = fastPCA(shape_matrix, 100, 1);
 %Get score to whole dataset
 mu = mean(shape_matrix);
 Xm =  bsxfun(@minus, shape_matrix, mu);
@@ -98,9 +102,9 @@ axis off;
 
 %Saving files
 cd(folder.save)
-cell2csv("landmark_ids.csv", landmark_ids)
-csvwrite("means.csv", mu')
-csvwrite("cs.csv", cs') 
+cell2csv("landmark_ids.txt", landmark_ids)
+csvwrite("means.txt", mu')
+csvwrite("cs.txt", cs') 
 csvwrite("scores.csv", total_scores) 
 csvwrite("eigenvalues.csv", diag(S) ) 
 csvwrite("eigenvectors.csv", V ) 
